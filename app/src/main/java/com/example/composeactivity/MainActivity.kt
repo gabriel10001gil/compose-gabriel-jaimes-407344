8708038cf0490c22c7317d629d2dfd21c5418f03
gabriel10001gil <jaimesgilgabrielfelipe@gmail.com>
2025-10-14 21:11:04 -0500

    Completed guided example for section 3.04 (Basic Navigation)

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -47,55 +47,96 @@ object Destinations {
 fun Activity3_04Screen(modifier: Modifier = Modifier) {
     // Controlador de navegación
     val navController = rememberNavController()
+    package com.example.composeactivity
 
-    // NavHost define el grafo de navegación
-    NavHost(
-        navController = navController,
-        startDestination = Destinations.SCREEN_A,
-        modifier = modifier.fillMaxSize()
-    ) {
-        composable(Destinations.SCREEN_A) {
-            ScreenA(onNavigateToB = { navController.navigate(Destinations.SCREEN_B) })
-        }
-        composable(Destinations.SCREEN_B) {
-            ScreenB(onNavigateBack = { navController.popBackStack() })
-        }
-    }
-}
 
-@Composable
-fun ScreenA(onNavigateToB: () -> Unit) {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Estás en la Pantalla A")
-        Button(onClick = onNavigateToB) {
-            Text("Ir a Pantalla B")
-        }
-    }
-}
 
-@Composable
-fun ScreenB(onNavigateBack: () -> Unit) {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Estás en la Pantalla B")
-        Button(onClick = onNavigateBack) {
-            Text("Volver")
-        }
-    }
-}
+    import android.os.Bundle
 
-@Preview(showBackground = true)
-@Composable
-fun Activity3_04Preview() {
-    ComposeActivityTheme {
-        // Para la preview, mostramos la pantalla inicial
-        ScreenA(onNavigateToB = {})
+            import androidx.activity.ComponentActivity
+
+            import androidx.activity.compose.setContent
+
+            import androidx.activity.enableEdgeToEdge
+
+            import androidx.compose.foundation.layout.fillMaxSize
+
+            import androidx.compose.foundation.layout.padding
+
+            import androidx.compose.material3.Scaffold
+
+            import androidx.compose.material3.Text
+
+            import androidx.compose.runtime.Composable
+
+            import androidx.compose.ui.Modifier
+
+            import androidx.compose.ui.tooling.preview.Preview
+
+            import com.example.composeactivity.ui.theme.ComposeActivityTheme
+
+
+
+    class MainActivity : ComponentActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+
+            super.onCreate(savedInstanceState)
+
+            enableEdgeToEdge()
+
+            setContent {
+
+                ComposeActivityTheme {
+
+                    Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+
+                        Greeting(
+
+                            name = "Android",
+
+                            modifier = Modifier.padding(innerPadding)
+
+                        )
+
+                    }
+
+                }
+
+            }
+
+        }
+
     }
-}
+
+
+
+    @Composable
+
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+        Text(
+
+            text = "Hello $name!",
+
+            modifier = modifier
+
+        )
+
+    }
+
+
+
+    @Preview(showBackground = true)
+
+    @Composable
+
+    fun GreetingPreview() {
+
+        ComposeActivityTheme {
+
+            Greeting("Android")
+
+        }
+
+    }
